0245f08fe5cb659e072b35b7c570bbc61727963a
Ibai <ibai.gorordo@hotmail.com>
2022-07-03 21:59:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ONNX HRNET Human Pose Estimation (WIP)
+ Python scripts for performing 2D human pose estimation using the HRNET family models in ONNX.
---
+++ image_pose_estimation.py
@@ -0,0 +1,23 @@
+import cv2
+from imread_from_url import imread_from_url
+
+from HRNET import HRNET, ModelType
+
+# Initialize inference model
+model_path = "models/hrnet_coco_w48_384x288.onnx"
+model_type = ModelType.COCO
+hrnet = HRNET(model_path, model_type, conf_threshold=0.0)
+
+# Read image
+img_url = "https://upload.wikimedia.org/wikipedia/commons/5/53/People_of_Tibet54.jpg"
+img = imread_from_url(img_url)
+
+# Perform the inference in the image
+total_heatmap, peaks = hrnet(img)
+
+# Draw Model Output
+output_img = hrnet.draw_pose(img)
+cv2.namedWindow("Model Output", cv2.WINDOW_NORMAL)
+cv2.imshow("Model Output", output_img)
+cv2.imwrite("doc/img/output.jpg", output_img)
+cv2.waitKey(0)
